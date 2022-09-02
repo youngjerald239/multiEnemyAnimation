b1f5a8079ee391a271b4756acb0b24fb5df6f8bf
youngjerald239 <youngjerald239@gmail.com>
2022-09-02 13:43:53 -0500

    adjusted ghost movement

--- script.js
+++ script.js
@@ -81,10 +81,17 @@ document.addEventListener('DOMContentLoaded', function(){
             this.y = Math.random() * this.game.height * 0.6
             this.image = ghost 
             this.vx = Math.random() * 0.2 + 0.1
+            this.angle = 0
+            this.curve = Math.random() * 3
+        }
+        update(deltaTime){
+            super.update(deltaTime)
+            this.y += Math.sin(this.angle) * this.curve
+            this.angle+= 0.04
         }
         draw(){
             ctx.save()
-            ctx.globalAlpha = 0.5
+            ctx.globalAlpha = 0.7
             super.draw(ctx)
             ctx.restore()
         }
