9dffc7e7743c8d14393a45bf95bf2eb09563685b
youngjerald239 <youngjerald239@gmail.com>
2022-09-02 13:02:36 -0500

    created worm subclass

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function(){
             
         }
         update(deltaTime){
+            this.enemies = this.enemies.filter(object => !object.markedForDeletion)
             if (this.enemyTimer  > this.enemyInterval){
                 this.#addNewEnemy()
                 this.enemyTimer = 0
@@ -28,25 +29,37 @@ document.addEventListener('DOMContentLoaded', function(){
             this.enemies.forEach(object => object.draw(this.ctx))
         }
         #addNewEnemy(){
-            this.enemies.push(new Enemy(this))
+            this.enemies.push(new Worm(this))
         }
     }
 
     class Enemy {
         constructor(game){
             this.game = game
-            console.log(this.game)
-            this.x = this.game.width
-            this.y = Math.random() * this.game.height
-            this.width = 100
-            this.height = 100
+            // console.log(this.game)
+            this.markedForDeletion = false
         }
         update(){
             this.x--
+            // remove enemies
+            if (this.x < 0 - this.width) this.markedForDeletion = true; 
         }
         draw(ctx){
-            ctx.fillRect(this.x, this.y, this.width, this.height)
+            // ctx.fillRect(this.x, this.y, this.width, this.height)
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
         }
+    }
+
+    class Worm extends Enemy {
+        constructor(game){
+            super(game)
+            this.x = this.game.width
+            this.y = Math.random() * this.game.height
+            this.width = 200
+            this.height = 100
+            this.image = worm 
+        }
+
     }
 
     const game = new Game(ctx, canvas.width, canvas.height);
